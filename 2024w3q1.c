8bd78f466272976a20c4e63513609ef2e61fde86
vestata <tony268596@gmail.com>
2024-03-30 18:41:15 +0800

    Implement `fls` function as alternative to `__builtin_clz`
    For the [Week 2 Quiz](https://hackmd.io/@sysprog/linux2024-quiz2),
    implemented `fls` (find last set bit) using a binary search-like
    method, mirroring the functionality of `__builtin_clz` (count
    leading zeros). This implementation makes the program
    independent of GNU extensions.

--- 2024w3q1.c
+++ 2024w3q1.c
@@ -1,13 +1,24 @@
 #include <math.h>
 #include <stdio.h>
 
+int fls(int word) {
+    int r = 32;
+    if (!word) return 0;
+    if (!(word & 0xFFFF0000u)) { word <<= 16; r -= 16; }
+    if (!(word & 0xFF000000u)) { word <<= 8; r -= 8; }
+    if (!(word & 0xF0000000u)) { word <<= 4; r -= 4; }
+    if (!(word & 0xC0000000u)) { word <<= 2; r -= 2; }
+    if (!(word & 0x80000000u)) { r -= 1; }
+    return r;
+}
+
 int i_sqrt(int x)
 {
     if (x <= 1) /* Assume x is always positive */
         return x;
 
     int z = 0;
-    for (int m = 1UL << ((31 - __builtin_clz(x)) & ~1UL); m; m >>= 2) {
+    for (int m = 1UL << ((31 - fls(x)) & ~1UL); m; m >>= 2) {
         int b = z + m;
         z >>= 1;
         if (x >= b)
