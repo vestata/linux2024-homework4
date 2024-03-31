356e76991fddd79e3317b17bce55b7a5c744de08
vestata <tony268596@gmail.com>
2024-03-31 22:40:59 +0800

    Add `ceil_ilog2` function in 2024w3q5.c for log2 ceiling
    Implemented `ceil_ilog2` in 2024w3q5.c to calculate the ceiling
    of the binary logarithm of a 32-bit unsigned integer. This
    function efficiently computes the log2 ceiling using bitwise
    shifts and comparisons, providing a compact and fast method for
    this mathematical operation.

---
+++ 2024w3q5.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdint.h>
+
+typedef unsigned int uint32_t;
+
+int ceil_ilog2(uint32_t x)
+{
+    uint32_t r, shift;
+
+    x--;
+    r = (x > 0xFFFF) << 4;                                                                                                                                    
+    x >>= r;
+    shift = (x > 0xFF) << 3;
+    x >>= shift;
+    r |= shift;
+    shift = (x > 0xF) << 2;
+    x >>= shift;
+    r |= shift;
+    shift = (x > 0x3) << 1;
+    x >>= shift;
+    return (r | shift | x > 1) + 1;       
+}
+
+int main(){
+    ceil_ilog2(36);
+    return 0;
+}
